bc480f3b33196906835368c237be146c72f6aa11
KOtiyaReba <knowledge.officer3@ictkerala.org>
2024-11-14 17:36:58 +0530

    first

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+require('./DB/connection');
+const cors = require('cors');
+const userRoute = require('./Routes/userRoutes');
+const postroute = require('./Routes/postRoute');
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/api',userRoute)
+app.use('/api',postroute);
+
+app.use(express.static('./frontend/build'));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+});
+
+app.listen(PORT,()=>{
+    console.log(`${PORT} is up and running`);
+})
